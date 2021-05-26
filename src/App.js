bf8f015e755c359743a243547f72757eb12b1e90
Artur Fidelis <65771411+arturfidelis-avoubiclabs@users.noreply.github.com>
2021-05-26 20:08:28 -0300

    filmaria 1.0

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import Routes from './routes';
+
+// https://sujeitoprogramador.com/r-api/?api=filmes/
+
+export default function App() {
+  return (
+    <div className="app">
+      <Routes/>
+    </div>
+  );
+}
