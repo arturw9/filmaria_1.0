eb8cfa27f15ab1dba2fe77f4d14fabbab5901ff0
Artur Fidelis <65771411+arturfidelis-avoubiclabs@users.noreply.github.com>
2021-05-27 00:39:15 -0300

    Pagina not-found

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import './favoritos.css';
@@ -8,41 +9,42 @@ export default function Favoritos(){
 
   useEffect(()=>{
 
-    const minhaLista = localStorage.getItem('Filmes');
+    const minhaLista = localStorage.getItem('filmes');
     setFilmes(JSON.parse(minhaLista) || []);
 
   }, []);
 
   function handleDelete(id){
-     let filtroFilmes = filmes.filter((item)=> {
+    let filtroFilmes = filmes.filter((item)=> {
       return (item.id !== id)
-     })
+    })
 
-     setFilmes(filtroFilmes);
-     localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
-     toast.success('Filme excluido com sucesso');
+    setFilmes(filtroFilmes);
+    localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    toast.success('Filme excluido com sucesso!');
+
   }
 
   return(
     <div id="meus-filmes">
       <h1>Meus Filmes</h1>
 
-      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :(</span> }
+      {filmes.length === 0 && <span>Você nao possui nenhum filme salvo :( </span> }
 
-    <ul>
-      {filmes.map((item)=>{
-        return(
-          <li key={item.id}>
-            <span>{item.id}</span>
+      <ul>
+        {filmes.map((item)=>{
+          return(
+            <li key={item.id}>
+              <span>{item.nome}</span>
 
-            <div>
-              <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-              <button onClick={ () => handleDelete(item.id)}>Excluir</button>
-            </div>
-          </li>
-        )
-      })}
-    </ul>
+              <div>
+                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
+                <button onClick={ () => handleDelete(item.id) }>Excluir</button>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
